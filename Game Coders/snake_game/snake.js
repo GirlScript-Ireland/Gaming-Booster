ebe145a8baf3f3d42cb437a104d0ca68cedab45c
noobie10123 <57508390+noobie10123@users.noreply.github.com>
2020-08-22 15:16:07 +0530

    made snake head

--- Game Coders/snake_game/snake.js
+++ Game Coders/snake_game/snake.js
@@ -1,5 +1,17 @@
 function Snake() {
   
+  this.x=0;
+  this.y=0;
+  this.xspeed=1;
+  this.yspeed=1; 
   
+  this.show=function(){
+    stroke(0,0,0);
+    strokeWeight(2);
+    fill(255);
+    rect(this.x,this.y,scl,scl);    
+  };
 }
+
+
   
