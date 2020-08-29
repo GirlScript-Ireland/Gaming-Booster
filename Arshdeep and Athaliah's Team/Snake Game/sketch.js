5e05fb8af791d63265098215586b344e38f071ee
Arshdeep Singh <arshdeep.singh@ucdconnect.ie>
2020-08-29 12:17:04 +0100

    Snake Game Done

--- Arshdeep and Athaliah's Team/Snake Game/sketch.js
+++ Arshdeep and Athaliah's Team/Snake Game/sketch.js
@@ -1,58 +1,58 @@
 let snake;
 let rez = 20;
-let food; 
+let food;
 let w;
 let h;
 
 function setup() {
   createCanvas(400, 400);
-  snake = new Snake();
-  frameRate(5);
   // The usage of let varibale
   // var x = 2; 
   // {
   //   let x = 10; 
   // }
   // print(x);
-  w = floor(width/ rez); 
-  h = floor(height/rez);
-  foodLocation()
-  
+  w = floor(width / rez);
+  h = floor(height / rez);
+  frameRate(5);
+  snake = new Snake();
+  foodLocation();
 }
 
-function foodLocation(){
+function foodLocation() {
   let x = floor(random(w));
   let y = floor(random(h));
   food = createVector(x, y);
 }
 
-
-
-function keyPressed(){
-  if (keyCode == LEFT_ARROW){
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
     snake.setDir(-1, 0);
-  }
-  else if (keyCode == RIGHT_ARROW){
+  } else if (keyCode === RIGHT_ARROW) {
     snake.setDir(1, 0);
-  }
-  else if (keyCode == DOWN_ARROW){
+  } else if (keyCode === DOWN_ARROW) {
     snake.setDir(0, 1);
-  }
-  else if (keyCode == UP_ARROW){
+  } else if (keyCode === UP_ARROW) {
     snake.setDir(0, -1);
   }
 }
 
-
 function draw() {
+  scale(rez);
   background(220);
-  if (snake.eat(food)){
+  if (snake.eat(food)) {
     foodLocation();
   }
-  scale(rez);
   snake.update();
   snake.show();
-  
-  fill(0, 0, 255);
+
+  if (snake.endGame()) {
+    print('END GAME');
+    background(255, 0, 0);
+    noLoop();
+  }
+
+  noStroke();
+  fill(255, 0, 0);
   rect(food.x, food.y, 1, 1);
 }
--- Arshdeep and Athaliah's Team/Snake Game/snake.js
+++ Arshdeep and Athaliah's Team/Snake Game/snake.js
@@ -1,38 +1,61 @@
-class Snake{
+class Snake {
+  constructor() {
+    this.body = [];
+    this.body[0] = createVector(floor(w / 2), floor(h / 2));
+    this.xdir = 0;
+    this.ydir = 0;
+    this.len = 0;
+  }
 
-    constructor(){
-      this.body = [];
-      this.body[0] = createVector(0,0);
-      this.xdir = 0;
-      this.ydir = 0;
+  setDir(x, y) {
+    this.xdir = x;
+    this.ydir = y;
+  }
+
+  update() {
+    let head = this.body[this.body.length - 1].copy();
+    this.body.shift();
+    head.x += this.xdir;
+    head.y += this.ydir;
+    this.body.push(head);
+  }
+
+  grow() {
+    let head = this.body[this.body.length - 1].copy();
+    this.len++;
+    this.body.push(head);
+  }
+
+  endGame() {
+    let x = this.body[this.body.length - 1].x;
+    let y = this.body[this.body.length - 1].y;
+    if (x > w - 1 || x < 0 || y > h - 1 || y < 0) {
+      return true;
     }
-    
-    setDir(x, y){
-      this.xdir = x;
-      this.ydir = y;
-    }
-    
-    update() {
-      this.body[0].x += this.xdir;
-      this.body[0].y += this.ydir;
-    }
-    
-    show(){
-      fill(0);
-      noStroke();
-      rect(this.body[0].x, this.body[0].y, 1, 1)
-    
-    }
-    
-    eat(pos){
-      let x = this.body[0].x;
-      let y = this.body[0].y;
-      if (x == pos.x && y == pos.y){
-        print("FOOD has been eaten");
+    for (let i = 0; i < this.body.length - 1; i++) {
+      let part = this.body[i];
+      if (part.x == x && part.y == y) {
         return true;
       }
-      return false;
     }
-    
-  
+    return false;
   }
+  
+  eat(pos) {
+    let x = this.body[this.body.length - 1].x;
+    let y = this.body[this.body.length - 1].y;
+    if (x == pos.x && y == pos.y) {
+      this.grow();
+      return true;
+    }
+    return false;
+  }
+
+  show() {
+    for (let i = 0; i < this.body.length; i++) {
+      fill(0);
+      noStroke();
+      rect(this.body[i].x, this.body[i].y, 1, 1);
+    }
+  }
+}
